e20a91b6f1c2a0676c80b8c0284f3ef35e5f303f
Peyton <peyton.kiel72@gmail.com>
2021-10-26 19:28:00 -0600

    loop constant checking for objects in bucket

--- main.py
+++ main.py
@@ -1,39 +1,54 @@
 import boto3
 import json
 from boto3 import client
+from time import sleep
 
 s3 = boto3.resource('s3')
+k = 2
+
+def create():
+
+    for j in keyList:
+        try:
+            content_object = s3.Object('usu-cs5260-peytonkiel-requests', str(j))
+            file_content = content_object.get()['Body'].read().decode('utf-8')
+            json_content = json.loads(file_content)
+            oname = json_content['owner'].lower().replace(" ", "-")
+            wid = json_content['widgetId']
+
+            keyName = "widgets/" + oname + "/" + wid
+            
+            copy_source = {
+            'Bucket': 'usu-cs5260-peytonkiel-requests',
+            'Key': str(j)
+            }
+
+            s3.meta.client.copy(copy_source, 'usu-cs5260-peytonkiel-web', keyName)
+            print("Created Object in Bucket: usu-cs5260-peytonkiel-web (Key: " + keyName + ")")
+
+            s3.Object('usu-cs5260-peytonkiel-requests', str(j)).delete()
+            
+
+        except KeyError:
+            print('ERROR: CREATION OF OBJECT CONSUMED AT KEY ' + j + " FAILED")
+            s3.Object('usu-cs5260-peytonkiel-requests', str(j)).delete()
+
+    keyList.clear()
 
 
-keyList = []
-conn = client('s3')
-for key in conn.list_objects(Bucket='usu-cs5260-peytonkiel-requests')['Contents']:
-    keyList.append(key['Key'])
 
-
-keyList.sort(key = int)
-
-
-
-for j in keyList:
+while k == 2:
     try:
-        content_object = s3.Object('usu-cs5260-peytonkiel-requests', str(j))
-        file_content = content_object.get()['Body'].read().decode('utf-8')
-        json_content = json.loads(file_content)
-        oname = json_content['owner'].lower().replace(" ", "-")
-        wid = json_content['widgetId']
+        keyList = []
+        conn = client('s3')
+        for key in conn.list_objects(Bucket='usu-cs5260-peytonkiel-requests')['Contents']:
+            keyList.append(key['Key'])
 
-        keyName = "widgets/" + oname + "/" + wid
+        keyList.sort(key = int)
+
+        create()
+
         
-        copy_source = {
-        'Bucket': 'usu-cs5260-peytonkiel-requests',
-        'Key': str(j)
-        }
-
-        s3.meta.client.copy(copy_source, 'usu-cs5260-peytonkiel-web', keyName)
-        print("Created Object in Bucket: usu-cs5260-peytonkiel-web (Key: " + keyName + ")")
-
-        s3.Object('usu-cs5260-peytonkiel-requests', str(j)).delete()
-
     except KeyError:
-        print('ERROR: CREATION OF OBJECT CONSUMED AT KEY ' + j + " FAILED")
+        print("Empty Bucket")
+        sleep(1)
